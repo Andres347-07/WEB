8b575b4dd4996de95bcd2bad6eb841af077be7bf
DIEGO347P <diego347p@gmail.com>
2025-11-26 16:51:46 -0500

    fix: Corregir error de sintaxis en admin-panel.js

--- admin/controllers/admin-panel.js
+++ admin/controllers/admin-panel.js
@@ -1,5 +1,5 @@
-// admin-dashboard.js
-import { auth, db } from "../../js/core/firebase-init.js"; // se ajusto la ruta
+// admin-panel.js - VERSIÓN CORREGIDA
+import { auth, db } from "../../js/core/firebase-init.js";
 import { onAuthStateChanged } from "https://www.gstatic.com/firebasejs/11.4.0/firebase-auth.js";
 import { doc, getDoc } from "https://www.gstatic.com/firebasejs/11.4.0/firebase-firestore.js";
 
